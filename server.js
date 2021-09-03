bc631e514214f4163f1c815bd6fd804b2b749cfa
Rome Bell <romebell5@gmail.com>
2021-09-03 13:07:50 -0700

    add: files

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+const app = express();
+
+const SECRET_SESSION = process.env.SECRET_SESSION;
+
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use(layouts);
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
+});
+
+module.exports = server;
