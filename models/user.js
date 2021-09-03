8dfcb786df8e73465ee6c416dccf0d057ef3dd31
Rome Bell <romebell5@gmail.com>
2021-09-03 15:11:43 -0700

    Create env variable and add session and flash middleware

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcrypt');
+
 const {
   Model
 } = require('sequelize');
@@ -45,6 +47,26 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'User',
   });
 
-  
+  // Before a user is created, we are encrypting the password and using hash in its place
+  User.addHook('beforeCreate', (pendingUser) => { // pendingUser is user object that gets passed to DB
+    // Bcrypt is going to hash the password
+    let hash = bcrypt.hashSync(pendingUser.password, 12); // hash 12 times
+    pendingUser.password = hash; // this will go to the DB
+  });
+
+   // Check the password on Sign-In and compare it to the hashed password in the DB
+  User.prototype.validPassword = function(typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
+
+    return isCorrectPassword;
+  }
+
+  // return an object from the database of the user without the encrypted password
+  User.prototype.toJSON = function() {
+    let userData = this.get(); 
+    delete userData.password; // it doesn't delete password from database, only removes it. 
+    
+    return userData;
+  }
   return User; // add functions above
 };
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
+console.log(SECRET_SESSION);
 
 app.set('view engine', 'ejs');
 
@@ -11,6 +14,20 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
+
+app.use(flash());            // flash middleware
+
+app.use((req, res, next) => {
+  console.log(res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index');
