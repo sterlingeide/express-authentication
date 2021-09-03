cdedf56960ae015456b8af575e1afadb524cc5a6
Rome Bell <romebell5@gmail.com>
2021-09-03 15:44:59 -0700

    isLoggedIn: add middleware import into server

---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,8 @@
+function isLoggedIn(req, res, next) {
+    if (!req.user) {
+        req.flash('error', 'You must be signed in to access page');
+        res.redirect('/auth/login');
+    }
+}
+
+module.exports = isLoggedIn;
